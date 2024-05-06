37b6be2bae3b6ccb0d09743d38fb329d1c1426f5
Steph-en <appahstephen9@gmail.com>
2024-05-06 14:39:31 +0000

    Update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JobLocationsComponent } from '../../job-locations/job-locations.component';
 import { JobLocation } from '../../interface/job-location';
+import { AppService } from '../../services/app.service';
 
 @Component({
   selector: 'app-home',
@@ -491,6 +492,9 @@ export class HomeComponent {
           "Proactively solve the problems most important to the business"
         ]
       }
-    }
+    },
   ]
+
+  constructor(private appService: AppService) {}
+
 }
--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { JobLocation } from '../interface/job-location';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AppService {
 
-  constructor() { }
+export class AppService {
+  private jobsUrl = '/assets/data.json';
+
+  constructor(private http: HttpClient) { }
+
+  public getjobsUrl(): Observable<JobLocation[]> {
+    return this.http.get<JobLocation[]>(this.jobsUrl);
+  }
 }
